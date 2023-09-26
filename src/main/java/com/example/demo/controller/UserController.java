87845c8152252ce92ec5085b1800592a0905e33d
冷兴达 <648612680@qq.com>
2023-09-26 16:23:49 +0800

    mybatis-plus写删除查询， 配置日志输出

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -1,5 +1,8 @@
 package com.example.demo.controller;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.example.demo.entity.User;
 import com.example.demo.mapper.UserMapper;
 import com.example.demo.service.UserService;
@@ -26,7 +29,9 @@ public class UserController {
     @ApiOperation(value = "全量用户列表",notes = "这里可以写一些详细信息")
     @GetMapping
     public List<User> index() {
-        return userMapper.findAll();
+
+//        return userService.findAll();
+        return userService.list();
     }
 
     @ApiOperation(value = "新增或者修改用户信息",notes = "这里可以写一些详细信息")
@@ -39,20 +44,34 @@ public class UserController {
 
     @ApiOperation(value = "删除指定用户",notes = "这里可以写一些详细信息")
     @DeleteMapping("/{id}")
-    public int delete(@PathVariable Integer id) {
-        return userMapper.deleteById(id);
+    public boolean delete(@PathVariable Integer id) {
+
+//        return userMapper.deleteById(id);
+        return userService.removeById(id);
     }
 
     @ApiOperation(value = "分页查询用户信息",notes = "这里可以写一些详细信息")
     @GetMapping("/page")
-    public Map<String, Object> findPage(@ApiParam(value = "页码", required = true) @RequestParam Integer pageNum, @RequestParam Integer pageSize) {
-        pageNum = (pageNum - 1) * pageSize;
-        int total = userMapper.findTotal();
-        List<User> data = userMapper.findPage(pageNum, pageSize);
-        Map<String, Object> res = new HashMap<>();
-        res.put("data", data);
-        res.put("total", total);
-        return res;
+    public IPage<User> findPage(@ApiParam(value = "页码", required = true) @RequestParam Integer pageNum,
+                                        @RequestParam Integer pageSize,
+                                @ApiParam(value = "用户名", required = false) @RequestParam(required = false, defaultValue = "") String username) {
+//        pageNum = (pageNum - 1) * pageSize;
+//        int total = userMapper.findTotal();
+//        List<User> data = userMapper.findPage(pageNum, pageSize);
+//        Map<String, Object> res = new HashMap<>();
+//        res.put("data", data);
+//        res.put("total", total);
+//        return res;
+        IPage<User> page = new Page<>(pageNum, pageSize);
+        QueryWrapper<User> queryWrapper = new QueryWrapper<>();
+
+        // 参数非空
+        if(username != null && !username.isEmpty()) {
+            queryWrapper.like("username", username);
+        }
+        IPage<User> userPage = userService.page(page, queryWrapper);
+        return userPage;
+
     }
 }
 
--- src/main/java/com/example/demo/mapper/UserMapper.java
+++ src/main/java/com/example/demo/mapper/UserMapper.java
@@ -8,21 +8,21 @@ import java.util.List;
 
 @Mapper
 public interface UserMapper extends BaseMapper<User> {
-    @Select("select * from sys_user")
-    List<User> findAll();
-
-    @Insert("insert into sys_user(username, password, phone, address, nickname, email) " +
-            "values(#{username}, #{password}, #{phone}, #{address}, #{nickname}, #{email})")
-    int insert(User user);
-
-    int update(User user);
-    
-    @Delete("delete from sys_user where id=#{id}")
-    int deleteById(@Param("id") Integer id);
-
-    @Select("select * from sys_user limit #{pageNum}, #{pageSize}")
-    List<User> findPage(Integer pageNum, Integer pageSize);
-
-    @Select("select count(*) from sys_user")
-    int findTotal();
+//    @Select("select * from sys_user")
+//    List<User> findAll();
+//
+//    @Insert("insert into sys_user(username, password, phone, address, nickname, email) " +
+//            "values(#{username}, #{password}, #{phone}, #{address}, #{nickname}, #{email})")
+//    int insert(User user);
+//
+//    int update(User user);
+//
+//    @Delete("delete from sys_user where id=#{id}")
+//    int deleteById(@Param("id") Integer id);
+//
+//    @Select("select * from sys_user limit #{pageNum}, #{pageSize}")
+//    List<User> findPage(Integer pageNum, Integer pageSize);
+//
+//    @Select("select count(*) from sys_user")
+//    int findTotal();
 }
