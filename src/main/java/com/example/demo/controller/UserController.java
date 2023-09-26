cad105700a31c133b8655bad29507fdadf2f44fc
冷兴达 <648612680@qq.com>
2023-09-26 11:08:15 +0800

    swagger配置

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -3,6 +3,9 @@ package com.example.demo.controller;
 import com.example.demo.entity.User;
 import com.example.demo.mapper.UserMapper;
 import com.example.demo.service.UserService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -12,6 +15,7 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/user")
+@Api(tags="用户中心")
 public class UserController {
     @Autowired
     private UserMapper userMapper;
@@ -19,11 +23,13 @@ public class UserController {
     @Autowired
     private UserService userService;
 
+    @ApiOperation(value = "全量用户列表",notes = "这里可以写一些详细信息")
     @GetMapping
     public List<User> index() {
         return userMapper.findAll();
     }
 
+    @ApiOperation(value = "新增或者修改用户信息",notes = "这里可以写一些详细信息")
     @PostMapping
     public boolean save(@RequestBody User user) {
 
@@ -31,13 +37,15 @@ public class UserController {
         return userService.saveUser(user);
     }
 
+    @ApiOperation(value = "删除指定用户",notes = "这里可以写一些详细信息")
     @DeleteMapping("/{id}")
     public int delete(@PathVariable Integer id) {
         return userMapper.deleteById(id);
     }
 
+    @ApiOperation(value = "分页查询用户信息",notes = "这里可以写一些详细信息")
     @GetMapping("/page")
-    public Map<String, Object> findPage(@RequestParam Integer pageNum, @RequestParam Integer pageSize) {
+    public Map<String, Object> findPage(@ApiParam(value = "页码", required = true) @RequestParam Integer pageNum, @RequestParam Integer pageSize) {
         pageNum = (pageNum - 1) * pageSize;
         int total = userMapper.findTotal();
         List<User> data = userMapper.findPage(pageNum, pageSize);
--- src/main/java/com/example/demo/entity/User.java
+++ src/main/java/com/example/demo/entity/User.java
@@ -2,6 +2,8 @@ package com.example.demo.entity;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,8 +11,10 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @TableName(value="sys_user")
+@ApiModel(value = "用户信息", description = "用户参数")
 public class User {
     @TableId
+    @ApiModelProperty(value = "ID")
     private Integer id;
     private String username;
     @JsonIgnore // 忽略password 字段， 不在前端展示
